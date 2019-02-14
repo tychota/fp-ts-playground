26ba2a6cafbcf3d2322a92857be4ae471e673893
TychoTa <tycho.tatitscheff+ananke@gmail.com>
2019-02-14 18:19:06 +0100

    new repository

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  moduleDirectories: ["./node_modules", "./src"],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest"
+  }
+};
